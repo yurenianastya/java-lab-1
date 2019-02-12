e093a7108c65d10a669e8ed509c84bb6a827f6f5
Nsty <43321431+yurenianastya@users.noreply.github.com>
2019-02-12 09:59:07 +0200

    updated projectorScreen class

--- Implementation.java
+++ Implementation.java
@@ -1,13 +1,16 @@
 public class Implementation extends ProjectorScreen  {
     public static void main(String[] args) {
-        ProjectorScreen smartXiaomiScreen = new ProjectorScreen(13, 18, "Xiaomi", "Wall", 16.8, 5.2, "DD163", true);
+        ProjectorScreen smartXiaomiScreen = new ProjectorScreen(13, 18, "Xiaomi", "Wall", 16.8,
+                5.2, "DD163", true);
         ProjectorScreen iProjectorScreen = new ProjectorScreen();
-        ProjectorScreen anotherProjectorScreen = new ProjectorScreen(10, 13, "BenQ", "Standing/Wall");
+        ProjectorScreen anotherProjectorScreen = new ProjectorScreen(10, 13,
+                "BenQ", "Standing/Wall");
         System.out.println("First one: " + smartXiaomiScreen.toString());
         System.out.println("Second one: " + iProjectorScreen.toString());
         System.out.println("Third one: " + anotherProjectorScreen.toString());
         ProjectorScreen objectForResetting = new ProjectorScreen();
-        objectForResetting.resetValues(10,25,"Bosch","Standing", 19.0, 3.5, "GHOK28381", false);
+        objectForResetting.resetValues(10,25,"Bosch","Standing", 19.0,
+                3.5, "GHOK28381", false);
         System.out.println("Resetted values: "+objectForResetting.toString());
         printStaticPrice();
         ProjectorScreen objectForMethod = new ProjectorScreen();
--- ProjectorScreen.java
+++ ProjectorScreen.java
@@ -1,32 +1,65 @@
 public class ProjectorScreen {
-    public int height;
-    public int width;
-    public String manufacturer;
-    public String typeOfBracing;
-        private double diagonal;
-        private double thickness;
-            protected String model;
-            protected boolean isColourful;
-                static int price;
+    private int height;
+    private int width;
+    private String manufacturer;
+    private String typeOfBracing;
+    private double diagonal;
+    private double thickness;
+        protected String model;
+        protected boolean isColourful;
+            static int price;
 
     public ProjectorScreen() {
         //System.out.println("Default");
     }
 
     public ProjectorScreen(int height, int width, String manufacturer, String typeOfBracing) {
-        this();
+        this(height, width, manufacturer, typeOfBracing, 0.0, 0.0, null, false);
+    }
+    public ProjectorScreen(int height, int width, String manufacturer, String typeOfBracing,
+                           double diagonal, double thickness, String model, boolean isColourful) {
         this.height = height;
         this.width = width;
         this.manufacturer = manufacturer;
         this.typeOfBracing = typeOfBracing;
-    }
-    public ProjectorScreen(int height, int width, String manufacturer, String typeOfBracing, double diagonal, double thickness, String model, boolean isColourful) {
-        this(height, width, manufacturer, typeOfBracing);
         this.diagonal = diagonal;
         this.thickness = thickness;
         this.isColourful = isColourful;
         this.model = model;
     }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public String getTypeOfBracing() {
+        return typeOfBracing;
+    }
+
+    public void setTypeOfBracing(String typeOfBracing) {
+        this.typeOfBracing = typeOfBracing;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
     public double getDiagonal() {
         return diagonal;
     }
@@ -42,29 +75,32 @@ public class ProjectorScreen {
     public void setThickness(double thickness) {
         this.thickness = thickness;
     }
-
+    @Override
     public String toString() {
-        String Details =
-                "The Projector's Screen size is: "+height+"x"+width+"\n"+
-                "Its manufacturer is: "+manufacturer+"\n"+
-                "Its bracing type is: "+typeOfBracing+"\n"+
-                "It has a diagonal of : "+diagonal+"\n"+
-                "Its thickness is: "+thickness+"\n"+
-                "The model is: "+model+"\n"+
-                "Is it colourful?: "+isColourful+"\n";
-        return Details;
+        return "ProjectorScreen{" +
+                "height=" + height +
+                ", width=" + width +
+                ", manufacturer='" + manufacturer + '\'' +
+                ", typeOfBracing='" + typeOfBracing + '\'' +
+                ", diagonal=" + diagonal +
+                ", thickness=" + thickness +
+                ", model='" + model + '\'' +
+                ", isColourful=" + isColourful +
+                '}';
     }
-    public static void printStaticPrice(){
+
+    public static void printStaticPrice() {
         price = 450;
         System.out.println("\n"+"The static price is: "+price);
     }
-    
+
     public void printPrice() {
         price = 1938;
         System.out.println("\n"+"Another static price is: "+price);
     }
-    
-    public void resetValues(int height, int width, String manufacturer, String typeOfBracing, double diagonal, double thickness, String model, boolean isColourful) {
+
+    public void resetValues(int height, int width, String manufacturer, String typeOfBracing,
+                            double diagonal, double thickness, String model, boolean isColourful) {
         this.height = height;
         this.width = width;
         this.manufacturer = manufacturer;
